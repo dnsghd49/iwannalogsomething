64dbebfdf3d4eb43ce832b140bd1399db546747f
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-19 23:59:26 -0500

    added choices to loadMainPrompt funciton and tested it

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const { prompt } = require("inquirer");
+const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
 //const db = require("./db");
 //require("console.table");
@@ -8,4 +8,39 @@ run();
 function run() {
     const logoText = logo({ name: "Employee Manager" }).render();
     console.log(logoText);
+
+    loadMainPrompts();
+}
+
+async function loadMainPrompts() {
+    const { choice } = await prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "view all emp", 
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "test2", 
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "test3", 
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "test4", 
+                    value: "VIEW_EMPLOYEES"
+                },
+                
+            ]
+        }
+    ])
+
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+    }
 }
