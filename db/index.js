f7fdbc48dd61edf84b548e075208d6938ae585e5
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-22 18:47:20 -0500

    added viewByDepartment function, viewBymanager function, and add employee function. adding employee function tested

--- db/index.js
+++ db/index.js
@@ -3,16 +3,51 @@ const connection = require("./connection");
 
 // This way... I don't have to write db.function to my functions later
 class DB {
-    // Keeping a reference to the connection on the class in case we need it later
-    constructor(connection) {
-      this.connection = connection;
-    }
+  // Keeping a reference to the connection on the class in case we need it later
+  constructor(connection) {
+    this.connection = connection;
+  }
 
-    findAllEmp(){
-        return this.connection.query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
-          );
-    }
+  findAllEmp() {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+    );
+  }
+
+  // Find all departments, join with employees and roles and sum up utilized department budget
+  findAllDepartments() {
+    return this.connection.query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
+    );
+  }
+
+findAllEmployeesByDepartment(departmentId) {
+  return this.connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+    departmentId
+  );
+}
+
+findAllEmpByManager(managerId) {
+  return this.connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+    managerId
+  );
+}
+
+addEmployee(employee) {
+  return this.connection.query(
+    "INSERT INTO employee SET ?", employee
+  );
+}
+
+findAllRoles() {
+  return this.connection.query(
+    "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+  );
+}
+
+
 
 
 
--- index.js
+++ index.js
@@ -25,23 +25,23 @@ async function loadMainPrompts() {
                 },
                 {
                     name: "View All Employees By Department",
-                    value: ""
+                    value: "VIEW_BY_DEPARTMENT"
                 },
                 {
                     name: "View All Employees By Manager",
-                    value: ""
+                    value: "VIEW_BY_MANAGER"
                 },
                 {
                     name: "Add Employee",
-                    value: ""
+                    value: "ADD_EMPLOYEE"
                 },
                 {
                     name: "Remove Employee",
-                    value: ""
+                    value: "REMOVE_EMPLOYEE"
                 },
                 {
                     name: "Update Employee Role",
-                    value: ""
+                    value: "UPDATE_ROLE"
                 },
                 {
                     name: "Update Employee Manager",
@@ -79,11 +79,23 @@ async function loadMainPrompts() {
     switch (choice) {
         case "VIEW_EMPLOYEES":
             return viewEmployees();
+        case "VIEW_BY_DEPARTMENT":
+            return viewByDepartment();
+        case "VIEW_BY_MANAGER":
+            return viewByManager();
+        case "ADD_EMPLOYEE":
+            return addEmp();
+        case "REMOVE_EMPLOYEE":
+            return removeEmp();
+        case "":
+            return ;
+        case "":
+            return ;
     }
 }
 
-
-async function viewEmployees(){
+// Function to view all the employees
+async function viewEmployees() {
     const employee = await db.findAllEmp();
 
     console.log("\n");
@@ -91,3 +103,117 @@ async function viewEmployees(){
 
     loadMainPrompts();
 }
+
+// Function to view all the employees by department
+async function viewByDepartment() {
+    const departments = await db.findAllDepartments();
+
+    const departmentChoices = departments.map(({ id, name }) => ({
+        name: name,
+        value: id
+    }));
+
+    const { departmentId } = await prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Which department would you like to see employees for?",
+          choices: departmentChoices
+        }
+      ]);
+
+    const employees = await db.findAllEmployeesByDepartment(departmentId);
+
+    console.log("\n");
+    console.table(employees);
+
+    loadMainPrompts();
+}
+
+// Function to show employees under selected manager by their first and last name
+async function viewByManager() {
+    const managers = await db.findAllEmp();
+    
+    const managerChoices = managers.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+
+    const { managerId } = await prompt([
+        {
+          type: "list",
+          name: "managerId",
+          message: "Which employee do you want to see direct reports for?",
+          choices: managerChoices
+        }
+    ]);
+
+    const employees = await db. findAllEmpByManager(managerId);
+
+    console.log("\n");
+
+    // If statement for displaying error when user selects regualr employee" 
+    if (employees.length === 0) {
+        console.log("The selected employee has no direct reports");
+      } else {
+        console.table(employees);
+    }
+
+    loadMainPrompts();
+}
+
+// Function for add more employees
+async function addEmp () {
+    const employees = await db.findAllEmp();
+    const roles = await db.findAllRoles();
+
+    const employee = await prompt([
+      {
+        name: "first_name",
+        message: "What is the employee's first name?"
+      },
+      {
+        name: "last_name",
+        message: "What is the employee's last name?"
+      }
+    ]);
+  
+    const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+  
+    const { roleId } = await prompt({
+        type: "list",
+        name: "roleId",
+        message: "What is the employee's role?",
+        choices: roleChoices
+    });
+  
+    employee.role_id = roleId;
+  
+    const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+    managerChoices.unshift({ name: "None", value: null });
+  
+    const { managerId } = await prompt({
+        type: "list",
+        name: "managerId",
+        message: "Who is the employee's manager?",
+        choices: managerChoices
+    });
+  
+    employee.manager_id = managerId;
+  
+    await db.addEmployee(employee);
+  
+    console.log(
+      `Added ${employee.first_name} ${employee.last_name} to the database`
+    );
+  
+    loadMainPrompts();
+}
+
+
